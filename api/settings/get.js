ebedbae06447ba74a2ff84b8590ad003c1c4521f
ELASHMAWYDEV <elashmawydev@gmail.com>
2021-07-14 21:16:03 +0200

    finished settings routes

--- api/settings/get.js
+++ api/settings/get.js
@@ -1,6 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const SettingsModel = require("../../models/Settings.model");
 
+router.post("/", async (req, res) => {
+  try {
+    //Check roles
+    if (req.user.role != "admin")
+      return res.json({
+        status: false,
+        message: "ليس لديك صلاحية الاطلاع علي هذه البيانات",
+      });
 
+    const settings = await SettingsModel.findOne({});
+
+    if (!settings)
+      await SettingsModel.create({
+        location: { coordinates: [39.252349, 21.476755] },
+      });
+
+    if (!settings.location)
+      await SettingsModel.updateOne({
+        $set: {
+          location: { coordinates: [39.252349, 21.476755] },
+        },
+      });
+
+    return res.json({
+      status: true,
+      message: "تم استرجاع البيانات بنجاح",
+      data: { settings },
+    });
+  } catch (e) {
+    console.log(`Error in /api/settings/get: ${e.message}`, e);
+
+    if (!res.headersSent) {
+      res.json({
+        status: false,
+        message: "حدث خطأ ما ،يرجي اعادة المحاولة مرة أخري",
+      });
+    }
+  }
+});
 
 module.exports = router;
--- api/settings/update.js
+++ api/settings/update.js
@@ -1,6 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const SettingsModel = require("../../models/Settings.model");
 
+router.post("/", async (req, res) => {
+  try {
+    //Check roles
+    if (req.user.role != "admin")
+      return res.json({
+        status: false,
+        message: "ليس لديك صلاحية الاطلاع علي هذه البيانات",
+      });
 
+    const { lat, lng } = req.body;
+    if (!lat || !lng)
+      return res.json({ status: false, message: "يجب تحديد الموقع" });
+
+    await SettingsModel.updateOne({
+      $set: {
+        location: { coordinates: [lng, lat] },
+      },
+    });
+
+    const newSettings = await SettingsModel.findOne({});
+
+    return res.json({
+      status: true,
+      message: "تم استرجاع البيانات بنجاح",
+      data: { settings: newSettings },
+    });
+  } catch (e) {
+    console.log(`Error in /api/settings/get: ${e.message}`, e);
+
+    if (!res.headersSent) {
+      res.json({
+        status: false,
+        message: "حدث خطأ ما ،يرجي اعادة المحاولة مرة أخري",
+      });
+    }
+  }
+});
 
 module.exports = router;
